7da8035234a1cd63ec54da9ae73eb76f0264b7e2
webtesting22 <148150265+webtesting22@users.noreply.github.com>
2025-04-29 18:38:44 +0530

    Updated Some Css

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
                         <img src="/Images/ShaishyaLogo.png" alt="" />
                         <span className="StylingTextSpan"><b>Official Address:</b> Survey no. 569, Opp Sandesh Bungalow, Karnavati Road, Makarba Village, Ahmedabad, Gujarat 380054</span>
                         <span className="StylingTextSpan"><b>Phone Number:</b> +91 9979005797</span>
-                        <span className="StylingTextSpan"><b>Email:</b> shaishyapulsearena@gmail.com</span>
+                        <span className="StylingTextSpan"><b>Email: </b>shaishyapulsearena@gmail.com</span>
 
                     </div>
                 </div>
