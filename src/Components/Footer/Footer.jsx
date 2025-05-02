f61971a3b99dd079c4689d9cd9a6258e9c064dda
webtesting22 <148150265+webtesting22@users.noreply.github.com>
2025-05-02 14:48:09 +0530

    Updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,9 +7,9 @@ const Footer = () => {
                 <div>
                     <div>
                         <img src="/Images/ShaishyaLogo.png" alt="" loading="lazy"/>
-                        <span className="StylingTextSpan"><b>Official Address:</b> Survey no. 569, Opp Sandesh Bungalow, Karnavati Road, Makarba Village, Ahmedabad, Gujarat 380054</span>
-                        <span className="StylingTextSpan"><b>Phone Number:</b> +91 9979005797</span>
-                        <span className="StylingTextSpan"><b>Email: </b>shaishyapulsearena@gmail.com</span>
+                        <span className="StylingTextSpan"><b>Official Address:</b> <a href="https://maps.app.goo.gl/zQGFjJe5aWSG51nT8" style={{color:"black"}} target="_blank" rel="noopener noreferrer">Survey no. 569, Opp Sandesh Bungalow, Karnavati Road, Makarba Village, Ahmedabad, Gujarat 380054</a></span>
+                        <span className="StylingTextSpan"><b>Phone Number:</b> <a href="tel:+919979005797" style={{color:"black"}}>+91 9979005797</a></span>
+                        <span className="StylingTextSpan"><b>Email: </b><a href="mailto:shaishyapulsearena@gmail.com" style={{color:"black"}}>shaishyapulsearena@gmail.com</a></span>
 
                     </div>
                 </div>
